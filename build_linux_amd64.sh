d7f3cea89e6d29fd45e29ec51f70b9f6c8a151d1
lishimeng <316527907@qq.com>
2021-08-02 17:07:18 +0800

    build_script

--- build_linux_amd64.sh
+++ build_linux_amd64.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 targetDir="pkg"
-appName="owl-messager"
+appName="openapi"
 GOOS=linux GOPROXY=https://goproxy.io GOARCH=amd64 go build -o "${targetDir}/${appName}" cmd/${appName}/main.go
